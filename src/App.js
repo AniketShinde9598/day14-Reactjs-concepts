ceb672e29295c4c486ad45f13ef52f2bdfd39215
AniketShinde9598 <aniketshinde9598@gmail.com>
2023-05-24 15:35:57 +0530

    commit.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import TodoOps from "./components/TodoOps";
+import React, { useState } from "react";
+import BmiScore from "./bmicomponents/BmiScore";
+import "./App.css";
 
 function App() {
+  const [bmi, setbmi] = useState(18);
+
   return (
-    <div>
-      <TodoOps />
+    <div className="App">
+      <h2>Welcome to React</h2>
+      <h4>BMI : {bmi}</h4>
+      <button onClick={() => setbmi(bmi + 5)}>Click</button>
+      <BmiScore Mybmi={bmi} />
     </div>
   );
 }
---
+++ src/bmicomponents/BmiScore.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function BmiScore(props) {
+  return <div>BmiScore : {props.Mybmi} </div>;
+}
+
+export default BmiScore;
